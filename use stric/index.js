174bae2837c15d5f359b56a1bdff090a98499418
kasunicts48 <kasundananjayaweerasooriya@gmail.com>
2022-06-14 20:42:41 +0530

    solved first problem in Hacker Rank

---
+++ use stric/index.js
@@ -0,0 +1,8 @@
+var re = "Msaws.X Mqasddr.X";
+if (re.search("Mr.") || re.search("Mrs.") || re.search("Ms.")||re.search("Dr.") || re.search("Er.")){
+    re=true;
+}else{
+    re=false;
+}
+
+console.log(re);
---
+++ use stric/solveMeFirst.js
@@ -0,0 +1,7 @@
+function solveMeFirst(a, b) {
+    // Hint: Type return a+b below   
+    return a+b;
+  }
+  
+  const output = solveMeFirst(5,3)
+  console.log(output);
