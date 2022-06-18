150392e0821e8f4cd38ec276256e338be235d246
kasunicts48 <kasundananjayaweerasooriya@gmail.com>
2022-06-19 02:26:10 +0530

    task -have to solve grid Challange

--- use stric/index.js
+++ use stric/index.js
@@ -1,6 +1,15 @@
 function gridChallenge(grid) {
     // Write your code here
-
+    var len = grid[0].length;
+    var ans = "";
+    for (let index = 0; index < grid.length; index++) {
+        if(grid[index].length === len){
+            return "YES"
+        }else{
+            return "NO"
+        }
+    }
 }
 
-console.log(gridChallenge());
+// gridChallenge(['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv'])
+console.log(gridChallenge(['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']));
